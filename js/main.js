221c538e20a157562757ff21ad07fd37c5baa0c7
ArkadiiVodolazskyi <jswift168@gmail.com>
2021-03-16 18:01:05 +0200

    Done: home page | 1920 -> 768px
    Next: home page | 768px -> 320px
    Other pages

--- js/main.js
+++ js/main.js
@@ -77,18 +77,38 @@ document.addEventListener("DOMContentLoaded", () => {
         <i class="fas fa-chevron-right"></i>
       </button>
     `,
-    // responsive: [
-    //   {
-    //     breakpoint: 1000,
-    //     settings: {
-    //       slidesToShow: 1,
-    //       slidesToScroll: 1,
-    //       arrows: true,
-    //       draggable: true,
-    //       touchThreshold: 300,
-    //     }
-    //   },
-    // ]
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          arrows: true,
+          draggable: true,
+          touchThreshold: 300,
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          arrows: true,
+          draggable: true,
+          touchThreshold: 300,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          arrows: false,
+          draggable: true,
+          touchThreshold: 300,
+        }
+      },
+    ]
   });
 });
 
